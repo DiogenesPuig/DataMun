7eb0d38838f618cae5c73019852dd233bd476aff
Federico Sironi <sironifede@gmail.com>
2021-06-11 20:06:08 -0300

    allowed host all

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2$p+l#+^g&$o6o*2q7w8+kcui7g)g-6e-w2xqjln@wkg#kt2#b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
