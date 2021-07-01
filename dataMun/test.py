be109d8a12c09a185c9137ca7cf316b4fdac452a
Federico Sironi <sironifede@gmail.com>
2021-07-01 18:19:59 -0300

    se logro hacer un execute a la base de datos

--- dataMun/test.py
+++ dataMun/test.py
@@ -1,5 +1,5 @@
 import openpyxl
-import pandas as pd
+
 import json
 import sqlite3
 
@@ -10,16 +10,24 @@ def workbookToJson(workbook):
     rows['rows'] = []
     for sheet in workbook.worksheets:
         fila = 0
-        for row in sheet.iter_rows(min_row=2,max_row=100,values_only=True):
+        for row in sheet.iter_rows(min_row=2,values_only=True):
+            existe = False
             cols = [''] * 21
             for i in range(len(cols)):
                 try:
                     cols[i] = str(row[i]).rstrip()
+                    if  cols[i] == "":
+                        cols[i] = "null"
+                    if not existe :
+                        if cols[i] == "None":
+                            existe = True
+                        if  "=SUBTOTAL" in cols[i]:
+                            existe = True
                 except:
-                    cols[i]=''
+                    cols[i] = "null"
             # ----
             try:
-                if row[4] != "TOTALES":
+                if row[4] != "TOTALES" and not existe:
                     rows['rows'].append({
                         'zona':cols[0],
                         'codigo_centro':cols[1],
@@ -52,27 +60,47 @@ def workbookToJson(workbook):
     return rows
 
 dict = workbookToJson(workbook)
-
+print("finalizado el el workbookToJson")
 conn = sqlite3.connect('../db.sqlite3')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS raw (zona VARCHAR(1),cod_centro INTEGER, center_name VARCHAR(20), cod_diag VARCHAR(10),diag VARCHAR(50), aM INTEGER,aF INTEGER, bM INTEGER,bF INTEGER, cM INTEGER,cF INTEGER, dM INTEGER,dF INTEGER, eM INTEGER,eF INTEGER, fM INTEGER,fF INTEGER, gM INTEGER,gF INTEGER, hM INTEGER,hF INTEGER);")
+c.execute("CREATE TABLE IF NOT EXISTS raw (zona INTEGER,cod_centro INTEGER, center_name VARCHAR(20), cod_diag VARCHAR(10),diag VARCHAR(50), aM INTEGER,aF INTEGER, bM INTEGER,bF INTEGER, cM INTEGER,cF INTEGER, dM INTEGER,dF INTEGER, eM INTEGER,eF INTEGER, fM INTEGER,fF INTEGER, gM INTEGER,gF INTEGER, hM INTEGER,hF INTEGER);")
 
-command = "insert into raw values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+command = "insert into raw values"
 val = ""
-
+i = 0
+length = len(dict['rows']) -1
+print(dict['rows'][0]["zona"])
 for row in dict['rows']:
+    
     val += "("
-    for col in row:
-        val += row[col] + ", "
-    val += ")"
-    val += val.replace(",)","),")
+    l = 0
+    for celda in row:
+        
+        if l == 2 or l== 3 or l == 4:
+            val += "'" + row[celda] + "',"
+        else:
+            if l == 20:
+                val += row[celda] 
+                break
+            else: 
+                val += row[celda] + ","
+        l += 1
+     
+    if i == length:
+        val += ")"
+        break
+    else:
+        val += "),"
 
-val += val.replace(",)",");")
+    i += 1
 
+val += ";"
 
-#c.execute(command,val)
-#conn.commit()
-#conn.close()
+print("finalizado el srting")
+command += val
+c.execute(command)
+conn.commit()
+conn.close()
 
 
 
