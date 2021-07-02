ba6869ef8222fdd76553790e8009c57194046c22
DiogenesPuig <48956813+DiogenesPuig@users.noreply.github.com>
2021-07-02 19:55:55 -0300

    Revert "Updating "

--- dataMun/filters.py
+++ dataMun/filters.py
@@ -5,9 +5,22 @@ from .models import *
 import datetime
 
 
+
+ages = [( '<1','<1 año'),
+    ( '1 a 5','1 a 5 años'),
+    ( '6 a 9','6 a 9 años'),
+    ( '10 a 14','10 a 14 años'),
+    ('15 a 19','15 a 19 años'),
+    ('20 a 54 ','20 a 54 años'),
+    ('55 a 64','55 a 64 años'),
+    ('65 y mas','65 y mas años')]
+sex = (
+       ('M', ('Masculino')),
+       ('F', ('Femenino')),
+       )
 class DiagnosticCasesFilter(django_filters.FilterSet):
-    age = django_filters.ModelMultipleChoiceFilter(queryset=Age.objects.all())
-    sex = django_filters.ModelMultipleChoiceFilter(queryset=Sex.objects.all())
+    age = django_filters.MultipleChoiceFilter(choices=ages)
+    sex = django_filters.MultipleChoiceFilter(choices=sex)
     week = django_filters.ModelMultipleChoiceFilter(queryset=Week.objects.filter(year=datetime.datetime.now().year))
     center = django_filters.ModelMultipleChoiceFilter(queryset=Center.objects.all())
     
