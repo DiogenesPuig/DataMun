acb1ff4226ab6c3fa9eebd13b8709c1a8be79203
DiogenesPuig <48956813+DiogenesPuig@users.noreply.github.com>
2021-07-02 19:59:03 -0300

    arreglando la views.py

--- dataMun/views.py
+++ dataMun/views.py
@@ -15,22 +15,19 @@ from django.conf import settings
 from django.forms.utils import ErrorList
 import datetime
 
-
 from rest_framework import generics
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import *
+
+
 # Create your views here.
-
-
 
 
 @login_required()
 def perfilView(request):
-    
     return render(request, "perfil.html")
-
 
 
 def logoutView(request):
@@ -55,7 +52,7 @@ def loginView(request):
                 return redirect('home')
         else:
             messages.error(request, "Usuario o Contraseña Incorrecta")
-            
+
             return render(request, 'login.html', context)
 
     return render(request, 'login.html', context)
@@ -78,25 +75,20 @@ def registerView(request):
     return render(request, 'register.html', context)
 
 
-
 def homeView(request):
-    
     context = {
-        
+
     }
-    return render(request, 'home.html',)
-
-
-
-
+    return render(request, 'home.html', )
 
 
 class PaceintePorDiagnostico():
-    def __init__(self,cantPacientePorDiagnostico,diagnostico):
+    def __init__(self, cantPacientePorDiagnostico, diagnostico):
         self.cantPacientePorDiagnostico = cantPacientePorDiagnostico
         self.diagnostico = diagnostico
 
-@user_passes_test(lambda u:u.is_staff)
+
+@user_passes_test(lambda u: u.is_staff)
 def diagnosticsView(request):
     try:
 
@@ -107,44 +99,39 @@ def diagnosticsView(request):
         for week in weeks:
             if week.week > max_week.week:
                 max_week = week
-        
+
         diagnostics = Diagnostic.objects.all()
         diagnostic_cases = DiagnosticCases.objects.all()
         alerts = []
         diagn_cod = []
-        
+
         for diagnostic in diagnostics:
             if len(alerts) != 100:
-                dots =  GetAlert(diagnostic_cases, diagnostic,max_week,year)
-                if dots.cases >= dots.top_rank and dots.cases != 0 and dots.week == max_week.week :
+                dots = GetAlert(diagnostic_cases, diagnostic, max_week, year)
+                if dots.cases >= dots.top_rank and dots.cases != 0 and dots.week == max_week.week:
                     if diagnostic.code not in diagn_cod:
-                        
-                        alerts.append({'diagnostic':diagnostic,'cases':dots.cases})
+                        alerts.append({'diagnostic': diagnostic, 'cases': dots.cases})
                         diagn_cod.append(diagnostic.code)
             else:
                 break
-        
 
-        
         context = {
-            'max_week':max_week,
-            'alerts':alerts, 
+            'max_week': max_week,
+            'alerts': alerts,
         }
     except:
         context = {}
-    return render(request, 'diagnostics.html',context)
+    return render(request, 'diagnostics.html', context)
 
 
-@user_passes_test(lambda u:u.is_staff)
-def diagnosticView(request,cod_diagnostic):
-    
+@user_passes_test(lambda u: u.is_staff)
+def diagnosticView(request, cod_diagnostic):
     try:
         diagnostic = Diagnostic.objects.get(code=cod_diagnostic)
         diagnostic_cases = DiagnosticCases.objects.filter(diagnostic=diagnostic)
-    
-        weeks =  Week.objects.all()
+
+        weeks = Week.objects.all()
         p = DiagnosticCasesFilter(request.GET, queryset=diagnostic_cases)
-        
 
         if request.GET:
             messages.info(request, "Filters aplied")
@@ -155,7 +142,6 @@ def diagnosticView(request,cod_diagnostic):
         if year == 0:
             year = 2021
 
-        
         centrosName = []
         centros = []
         centro = ""
@@ -166,105 +152,89 @@ def diagnosticView(request,cod_diagnostic):
                 if pac.center.name == centro:
                     suma += pac.cases
                 else:
-                
+
                     centrosName.append(centro)
-                    n = 'centro ' +centro
+                    n = 'centro ' + centro
                     co = {
                         'nombre': n,
-                        'cases':suma,
-                        'cod':pac.center.code,
+                        'cases': suma,
+                        'cod': pac.center.code,
                     }
-                    
+
                     centros.append(co)
-                    
+
                     centro = pac.center.name
-                
+
                     suma = 0
             if len(centrosName) >= 20:
                 break
-        
-        averages, cumulative = GetGraphicAverages(p.qs,diagnostic,weeks,year,3)
+
+        averages, cumulative = GetGraphicAverages(p.qs, diagnostic, weeks, year, 3)
         print('graphic 1 and 3 Ok')
-        quartiles = GetGraphicQuartiles(p.qs,diagnostic,weeks,year,3)
+        quartiles = GetGraphicQuartiles(p.qs, diagnostic, weeks, year, 3)
         print('graphic 2 Ok')
         context = {
-            'averages':averages,
-            'quartiles':quartiles,
-            'cumulative':cumulative,
-            'diagnostic':diagnostic,
+            'averages': averages,
+            'quartiles': quartiles,
+            'cumulative': cumulative,
+            'diagnostic': diagnostic,
             'filterP': p,
-            'google_api_key':settings.APY_KEY,
+            'google_api_key': settings.APY_KEY,
             'centros': centros,
         }
     except:
         context = {}
-    return render(request, 'diagnostic.html',context)
+    return render(request, 'diagnostic.html', context)
 
 
-
-
-@user_passes_test(lambda u:u.is_staff)
+@user_passes_test(lambda u: u.is_staff)
 def uploadFileView(request):
     form = CreateFileForm()
-    if request.method == "POST": # If the form has been submitted...
-        
-         # All validation rules pass
+    if request.method == "POST":  # If the form has been submitted...
+
+        # All validation rules pass
         form = CreateFileForm(request.POST, request.FILES)
         if form.is_valid():
-            
             file = form.save()
             file = SpreadSheet.objects.get(pk=file.id)
             success = insertWorkbook(file)
-            
 
-            
             """
-
-
             if len(success) != 1:
-
                 file = SpreadSheet.objects.get(pk=file.id)
-                #file.delete()
+                file.delete()
                 for error in success:
-                    print(error[1])
                     if error[0] == 1:
-
                         form._errors["tabla"] = ErrorList([u" El archivo subido debe ser excel."])
-
                     if error[0] == 2:
-                    
+
                         form._errors["tabla"] = ErrorList([u" Revisa que el titulo de la hoja este correcto con su año y semana"])
-                    
+
                     context = {
                         'form':form
                     }
 
-
-                
                 return render(request, 'uploadFile.html',context)"""
-
-                return render(request, 'uploadFile.html',context)
-
             messages.success(request, "El archivo " + str(file.file) + ' fue agregado')
-            return redirect('diagnostics') ## redirects to aliquot page ordered by the most recent
-        
+            return redirect('diagnostics')  ## redirects to aliquot page ordered by the most recent
+
+
+
     else:
-        form = CreateFileForm() # An unbound form
-        
+        form = CreateFileForm()  # An unbound form
+
     context = {
-        'form':form
+        'form': form
     }
-    return render(request, 'uploadFile.html',context)
+    return render(request, 'uploadFile.html', context)
 
 
-    
 def centersView(request):
-    return render(request,'centers.html')
-#django rest_fremework views
+    return render(request, 'centers.html')
 
 
+# django rest_fremework views
 class CenterView(generics.ListAPIView):
-
     queryset = Center.objects.all()
     serializer_class = CenterSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
@@ -276,7 +246,4 @@ class DiagnosticsView(generics.ListAPIView):
     serializer_class = DiagnosticSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = {'name': ['icontains']}
-
-
-
 
