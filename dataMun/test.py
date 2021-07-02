fcf008a08dc3b60bcd8f1009785a9cde2a4bd398
Federico Sironi <sironifede@gmail.com>
2021-07-01 22:07:37 -0300

    insert con los años y la semana

--- dataMun/test.py
+++ dataMun/test.py
@@ -3,102 +3,82 @@ import openpyxl
 import json
 import sqlite3
 
+
+def workbookToSqlStatements(workbook,table_name,sheet_name_0,sheet_name_splitter,sheet_name_1,cols_name,ints,length_varchar,min_row):
+    """
+    Returns create and insert sql statement's from a given workbook 
+    """
+    create_sql_statement = ""
+    create_sql_statement = f"CREATE TABLE IF NOT EXISTS {table_name} "
+    create_sql_statement += f"(id INTEGER PRIMARY KEY AUTOINCREMENT, {sheet_name_0} UNSIGNED INTEGER, {sheet_name_1} UNSIGNED INTEGER,"
+    
+    for i in range(len(cols_name)):
+        
+        if i in ints:
+            create_sql_statement += f"{cols_name[i]} UNSIGNED INTEGER"
+        else:
+            create_sql_statement += f"{cols_name[i]} VARCHAR({length_varchar})"
+        
+        if i != len(cols_name) - 1:
+            create_sql_statement += ","
+    create_sql_statement += ");"
+    cols = str(cols_name).strip("]")
+    cols = cols.strip("[")
+    insert_sql_statement = ""
+    insert_sql_statement = f"insert into {table_name} ({sheet_name_0},{sheet_name_1},{cols}) values "
+    for sheet in workbook.worksheets:
+        sheet_name_list = str(sheet.title).split(sheet_name_splitter)
+        year = sheet_name_list[0]
+        week = sheet_name_list[1]
+        for row in sheet.iter_rows(min_row=min_row,values_only=True):
+            values = "("
+            values += f"{year},{week}," 
+            insert = True
+            for i in range(len(cols_name)):
+                
+                try:
+                    
+                    if i in ints:
+                        
+                        cell = str(row[i]).rstrip()
+                        try:
+                            values += f"{int(cell)}"
+                        except:
+                            values += "null"
+                    else:
+                        if str(row[i]) != "" and str(row[i]) != "None":
+                            values += f"'{str(row[i]).rstrip()}'"
+                        else:
+                            insert = False
+                    
+                    if i != len(cols_name) - 1:
+                        values += ","
+                    else:  
+                        break
+                except:
+                    return print("error the length of the list cols_name exeed the length of max columns of the sheet")
+                
+            values += ")"
+
+            if insert:
+                insert_sql_statement += values + ",\n"
+
+    insert_sql_statement += ";"
+    insert_sql_statement = insert_sql_statement.replace(",\n;","\n;")
+    return create_sql_statement, insert_sql_statement
+
 workbook = openpyxl.load_workbook('Diagnosticos 1° vez por CS 2021 sem 5 a 12.xlsx')
 
-def workbookToJson(workbook):
-    rows = {}
-    rows['rows'] = []
-    for sheet in workbook.worksheets:
-        fila = 0
-        for row in sheet.iter_rows(min_row=2,values_only=True):
-            existe = False
-            cols = [''] * 21
-            for i in range(len(cols)):
-                try:
-                    cols[i] = str(row[i]).rstrip()
-                    if  cols[i] == "":
-                        cols[i] = "null"
-                    if not existe :
-                        if cols[i] == "None":
-                            existe = True
-                        if  "=SUBTOTAL" in cols[i]:
-                            existe = True
-                except:
-                    cols[i] = "null"
-            # ----
-            try:
-                if row[4] != "TOTALES" and not existe:
-                    rows['rows'].append({
-                        'zona':cols[0],
-                        'codigo_centro':cols[1],
-                        'centro_name':cols[2],
-                        'cod_diag':cols[3],
-                        'diag':cols[4],
-                        '<1M':cols[5],
-                        '<1F':cols[6],
-                        '1M':cols[7],
-                        '1F':cols[8],
-                        '2a5M':cols[9],
-                        '2a5F':cols[10],
-                        '6a10M':cols[11],
-                        '6a10F':cols[12],
-                        '11a14M':cols[13],
-                        '11a14F':cols[14],
-                        '15a19M':cols[15],
-                        '15a19F':cols[16],
-                        '20a60M':cols[17],
-                        '20a60F':cols[18],
-                        '61>M':cols[19],
-                        '61>F':cols[20]
-                    })
-                    #print(rows['rows'][fila])
-            except:
-                pass
-            fila+=1
-    #print(len(rows['rows']))
-
-    return rows
-
-dict = workbookToJson(workbook)
-print("finalizado el el workbookToJson")
 conn = sqlite3.connect('../db.sqlite3')
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS raw (zona INTEGER,cod_centro INTEGER, center_name VARCHAR(20), cod_diag VARCHAR(10),diag VARCHAR(50), aM INTEGER,aF INTEGER, bM INTEGER,bF INTEGER, cM INTEGER,cF INTEGER, dM INTEGER,dF INTEGER, eM INTEGER,eF INTEGER, fM INTEGER,fF INTEGER, gM INTEGER,gF INTEGER, hM INTEGER,hF INTEGER);")
+create_sql_statement, insert_sql_statement = workbookToSqlStatements(workbook,"raw","year"," ","week",["col0","col1","col2","col3","col4","col5","col6","col7","col8","col9","col10","col11","col12","col13","col14","col15","col16","col17","col18","col19","col20"],[0,1,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],70,2)        
 
-command = "insert into raw values"
-val = ""
-i = 0
-length = len(dict['rows']) -1
-print(dict['rows'][0]["zona"])
-for row in dict['rows']:
-    
-    val += "("
-    l = 0
-    for celda in row:
-        
-        if l == 2 or l== 3 or l == 4:
-            val += "'" + row[celda] + "',"
-        else:
-            if l == 20:
-                val += row[celda] 
-                break
-            else: 
-                val += row[celda] + ","
-        l += 1
-     
-    if i == length:
-        val += ")"
-        break
-    else:
-        val += "),"
-
-    i += 1
-
-val += ";"
-
-print("finalizado el srting")
-command += val
-c.execute(command)
+#print("finalizado el srting:",insert_sql_statement)
+#print(create_sql_statement)
+drop_table = "drop table if exists raw;"
+c.execute(drop_table)
+c.execute(create_sql_statement)
+c.execute(insert_sql_statement)
 conn.commit()
 conn.close()
 
